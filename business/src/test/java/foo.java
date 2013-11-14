6ad10c660389fad3ad4295e64e5d1e3a1195d9dd
Tiago Simões Batista <tiago-s-batista@ext.ptin.corppt.com>
2013-11-14 18:44:58 +0000

    Refactor project into web and business components
    This shows how projects can form a simple hierarchy (with inter
    dependencies) and how much simpler the modules pom becomes when using
    plugin and dependency management on the parent pom

---
+++ business/src/test/java/foo.java
@@ -0,0 +1,43 @@
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author tsbatista
+ */
+public class foo {
+    
+    public foo() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @BeforeMethod
+    public void setUpMethod() throws Exception {
+    }
+
+    @AfterMethod
+    public void tearDownMethod() throws Exception {
+    }
+}
